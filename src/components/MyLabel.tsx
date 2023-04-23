6160d6d43b4a8a6e432d3d2f0884d40b54c4290d
Ricardo Pérez <rpe19971210@gmail.com>
2023-04-23 00:08:11 -0400

    pasos con storybook

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,44 @@
+import './mylabel.css';
+
+interface MyLabelProps {
+    /**
+     * Este es el mensaje a mostrar en la etiqueta
+    */
+    label: string;
+
+    /**
+     * Este es el tamaño en la etiqueta
+    */
+    size: 'normal'|'h1'|'h2'|'h3';
+
+    /**
+     * Si quiere todo capitalizado
+    */
+    AllCaps?: boolean;
+
+    /**
+     * Colores básicos del bóton
+    */
+    color?: 'primary'|'secondary'|'tertiary';
+
+    /**
+     * Color personalizado de la fuente
+    */
+    fontColor?: string;
+}
+
+export const MyLabel = ({
+    AllCaps = false,
+    color   = 'primary',
+    label   = 'No Label',
+    size    = 'normal',
+    fontColor
+}: MyLabelProps ) => {
+    return (
+        <span 
+            className={`label ${ size } text-${ color }` }
+            style={{ color: fontColor }}>
+            { AllCaps ? label.toUpperCase() : label }
+        </span>
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { MyLabel } from "../../components/MyLabel";
+ 
+const meta = {
+    title: 'UI/MyLabel',
+    component: MyLabel,
+    tags: ['autodocs'],
+    argTypes: {
+        size:  { control: 'select' },
+        color: { control: 'select' },
+        fontColor: { control: 'color' }
+    }
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+    args: {
+        label: '',
+        size: 'normal',
+        AllCaps: false
+    },
+};
+
+export const AllCaps: Story = {
+    args: {
+        label: '',
+        size: 'normal',
+        AllCaps: true
+    },
+};
+
+export const Secondary: Story = {
+    args: {
+        label: '',
+        size: 'normal',
+        color: 'secondary'
+    },
+};
+
+export const Tertiary: Story = {
+    args: {
+        label: '',
+        size: 'normal',
+        color: 'tertiary'
+    },
+};
+
+export const CustomFontColor: Story = {
+    args: {
+        label: '',
+        size: 'h1',
+        fontColor: '#5517ac'
+    },
+};
